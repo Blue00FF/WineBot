e0bc054f3b0301975a7549e19eb6d2065a133ec7
Fabrizio Martone <53048926+Blue00FF@users.noreply.github.com>
2022-01-17 13:41:15 +0000

    Refactored the code
    Rewrote the code so that all the function definitions are in Winebot, the user only needs to extract the folder containing the data and not rename it / move the .csv files, and other little optimizations that make the code look cleaner and clearer.

--- Data_skimmer.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import nltk
-
-df = pd.read_csv('../data/winemag-data-130k-v2.csv', index_col=0)
-df = df.drop(['taster_name', 'taster_twitter_handle', 'region_1',
-             'region_2', 'province', 'winery'], axis=1)
-df = df.dropna()
-df.to_csv('../data/winemag-data_custom.csv', index=False)
